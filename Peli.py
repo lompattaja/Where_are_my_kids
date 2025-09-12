7e137b64cffad1c7ee90894bb095256b2a3dd8bf
Your Name <nooa.malka@gmail.com>
2025-09-12 14:38:08 +0300

    Toinen versio

--- Peli.py
+++ Peli.py
@@ -1,6 +1,11 @@
-#Peli alkaa
+import os
+def clear():
+    # Detect operating system and run the right command
+    os.system('cls' if os.name == 'nt' else 'clear')
+#peli alkaa
 print("Eräänä päivänä lentävä apina ja hänen kymmenen pientä lastaan olivat matkalla takaisin kotiin, Suomeen.")
 print("Taivas yllättäen tummui, ja heidän ylleen nousi raivokas myrsky.")
 print("Kovat tuulet tarttuivat pieniin apinanpoikasiin ja lennättivät heidät kauas, ympäri Euroopan maita.")
 print("Äitiapina kauhistui. Hän yritti tavoittaa lapsiaan, mutta tuulen voima oli liian suuri. ")
-print("")
+input("paina enter jaktaaksesi")
+clear()
