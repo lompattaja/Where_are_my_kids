0fd96454355235c06dd172ded997992c09365f9b
Helena <130378157+hellupellu@users.noreply.github.com>
2025-09-12 14:48:14 +0300

    Ensimmäinen versio

--- Peli.py
+++ Peli.py
@@ -2,10 +2,18 @@ import os
 def clear():
     # Detect operating system and run the right command
     os.system('cls' if os.name == 'nt' else 'clear')
-#peli alkaa
+
+#pelin tarina
 print("Eräänä päivänä lentävä apina ja hänen kymmenen pientä lastaan olivat matkalla takaisin kotiin, Suomeen.")
 print("Taivas yllättäen tummui, ja heidän ylleen nousi raivokas myrsky.")
 print("Kovat tuulet tarttuivat pieniin apinanpoikasiin ja lennättivät heidät kauas, ympäri Euroopan maita.")
-print("Äitiapina kauhistui. Hän yritti tavoittaa lapsiaan, mutta tuulen voima oli liian suuri. ")
-input("paina enter jaktaaksesi")
+print("Äitiapina kauhistui. Hän yritti tavoittaa lapsiaan, mutta tuulen voima oli liian suuri.")
+print("Kun myrsky vihdoin tyyntyi, jäljellä oli vain hiljainen taivas ja äidin sydäntä painava huoli.")
+print("Lentävä apina keräsi rohkeutensa ja hänen oli lähdettävä etsimään kadonneita lapsiaan.")
+print("Jokainen niistä saattoi olla missä päin Eurooppaa tahansa")
+print("ja vain sinä voisit auttaa häntä tässä vaikeassa tilanteessa.")
+print("Auttaisitko häntä pelaaja?")
+
+input("Paina enter jaktaaksesi: ")
 clear()
+
