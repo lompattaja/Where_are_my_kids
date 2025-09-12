40ccdf7b3a8afdc1f83f3b874e276f7b622beac9
Your Name <nooa.malka@gmail.com>
2025-09-12 14:17:41 +0300

    Ensimmäinen versio

---
+++ Peli.py
@@ -0,0 +1,6 @@
+#Peli alkaa
+print("Eräänä päivänä lentävä apina ja hänen kymmenen pientä lastaan olivat matkalla takaisin kotiin, Suomeen.")
+print("Taivas yllättäen tummui, ja heidän ylleen nousi raivokas myrsky.")
+print("Kovat tuulet tarttuivat pieniin apinanpoikasiin ja lennättivät heidät kauas, ympäri Euroopan maita.")
+print("Äitiapina kauhistui. Hän yritti tavoittaa lapsiaan, mutta tuulen voima oli liian suuri. ")
+print("")
