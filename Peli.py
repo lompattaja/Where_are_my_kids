faab1b45b97081d8188f66ec5ecb7aaead6ea4f0
Your Name <nooa.malka@gmail.com>
2025-09-19 10:05:36 +0300

    Tehokkaampi tilan käyttö

--- Peli.py
+++ Peli.py
@@ -4,15 +4,17 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 #pelin tarina
-print("Eräänä päivänä lentävä apina ja hänen kymmenen pientä lastaan olivat matkalla takaisin kotiin, Suomeen.")
-print("Taivas yllättäen tummui, ja heidän ylleen nousi raivokas myrsky.")
-print("Kovat tuulet tarttuivat pieniin apinanpoikasiin ja lennättivät heidät kauas, ympäri Euroopan maita.")
-print("Äitiapina kauhistui. Hän yritti tavoittaa lapsiaan, mutta tuulen voima oli liian suuri.")
-print("Kun myrsky vihdoin tyyntyi, jäljellä oli vain hiljainen taivas ja äidin sydäntä painava huoli.")
-print("Lentävä apina keräsi rohkeutensa ja hänen oli lähdettävä etsimään kadonneita lapsiaan.")
-print("Jokainen niistä saattoi olla missä päin Eurooppaa tahansa")
-print("ja vain sinä voisit auttaa häntä tässä vaikeassa tilanteessa.")
-print("Auttaisitko häntä pelaaja?")
+print("""Eräänä päivänä lentävä apina ja hänen kymmenen pientä lastaan olivat matkalla takaisin kotiin, Suomeen.
+Taivas yllättäen tummui, ja heidän ylleen nousi raivokas myrsky.
+
+Kovat tuulet tarttuivat pieniin apinanpoikasiin ja lennättivät heidät kauas, ympäri Euroopan maita.
+Äitiapina kauhistui. Hän yritti tavoittaa lapsiaan, mutta tuulen voima oli liian suuri.
+Kun myrsky vihdoin tyyntyi, jäljellä oli vain hiljainen taivas ja äidin sydäntä painava huoli.
+Lentävä apina keräsi rohkeutensa ja hänen oli lähdettävä etsimään kadonneita lapsiaan.
+Jokainen niistä saattoi olla missä päin Eurooppaa tahansa
+
+Vain sinä voisit auttaa häntä tässä vaikeassa tilanteessa.
+Auttaisitko häntä pelaaja?""")
 
 input("Paina enter jaktaaksesi: ")
 clear()
