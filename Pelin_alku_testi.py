597acb53d39e43c3bc6805e3b12b2a0582221586
Helena <130378157+hellupellu@users.noreply.github.com>
2025-10-01 12:35:10 +0300

    Peli siihen asti kunnes kysytään nimimerkki

--- Pelin_alku_testi.py
+++ Pelin_alku_testi.py
@@ -1,7 +1,48 @@
+import mysql.connector
 import os
 import time
 
-frames = [
+# Muodostetaan tietokantayhteys
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='salasana',
+         autocommit=True
+         )
+
+# Funktio, joka tyhjentää näytön (windows)
+def tyhjennä():
+    os.system("cls")
+
+def jatka():
+    print(input("""
+    
+Paina enter jatkaaksesi."""))
+    os.system("cls")
+
+# time.sleep(1) -> voi käyttää jos haluaa pitää taukoa
+
+# Pelin tarina
+print("Eräänä päivänä lentävä apina ja hänen kymmenen lastaan olivat matkalla takaisin kotiin.")
+jatka()
+print("Kovat tuulet tarttuivat pieniin apinanpoikasiin ja lennättivät heidät kauas pois, ympäri Euroopan maita.")
+jatka()
+print("""Äitiapina kauhistui.
+Hän yritti tavoittaa lapsiaan, mutta tuulen voima oli liian suuri.""")
+jatka()
+
+print("Kun myrsky vihdoin tyyntyi, jäljellä oli vain hiljainen taivas ja äidin sydäntä painava huoli.")
+jatka()
+print("Lentävä apina keräsi rohkeutensa ja hänen oli lähdettävä etsimään kadonneita lapsiaan.")
+jatka()
+print("""Jokainen niistä saattoi olla missä päin Eurooppaa tahansa
+ja vain sinä voisit auttaa häntä tässä vaikeassa tilanteessa.""")
+jatka()
+
+# Itkevä apina animaatio
+kuvat = [
     r"""        
                     ..::+*******+.:..               
                 .:********************:.            
@@ -123,7 +164,87 @@ frames = [
     """,
 ]
 
-for frame in frames:
-    os.system("cls" if os.name == "nt" else "clear")
-    print(frame)
-    time.sleep(0.5)
+for kuva in kuvat:
+    tyhjennä()
+    print(kuva)
+    time.sleep(0.6)             # kuvat tulee 0.6 sekunnin välein (näyttää kuin apina itkee)
+
+tyhjennä()
+
+# Pelin alku
+print("Auttaisitko häntä?")
+jatka()
+
+# Haetaan game-taulusta suurin id-arvo ja palautetaan se suurin arvo +1. (koska id:llä ei ole auto_increment)
+# id on varchar eli se on tekstikenttä ei numero, eli pitää palauttaa merkkijonona (str)
+def luo_uusi_id():
+    sql = "select max(id) from game"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()      # palauttaa yhden rivin
+    suurin_id = tulos[0]            # ylempi palauttaa tuplen eli esim. ('3',) ja halutaan vain se eka arvo eli pelkkä numero 3
+    uusi_id = int(suurin_id) + 1    # muutetaan suurin_id kokonaisluvuksi, koska se on string
+    return str(uusi_id)             # muutetaan se takaisin stringiksi
+
+# Lisätään uusi rivi game-tauluun kohtaan (id ja screen_name)
+def lisää_pelaaja(nimimerkki):
+    uusi_id = luo_uusi_id()         # hakee seuraavan id:n luo_uusi_id funktion avulla
+    sql = f"insert into game (id, screen_name) values ('{uusi_id}', '{nimimerkki}')"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    print(f"Kiitos, kun autat minua {nimimerkki}.")
+
+# Tarkistaa onko pelaajan antama nimimerkki jo käytössä
+# Palauttaa True jos löytyy ja False jos ei löydy
+def nimimerkki_käytössä(nimimerkki):
+    sql = f"select count(*) from game where screen_name = '{nimimerkki}'"    # count(*) kertoo monta riviä löytyy sillä nimimerkillä
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    return tulos[0] > 0         # sql palauttaa taas tuplen niin otetaan eka arvo numerona
+
+# PÄÄOHJELMA:
+print("Tervetuloa peliin.")
+
+# Kysytään onko uusi vai vanha pelaaja
+# Jos vanha pelaaja niin jatketaan vanhasta pelistä ellei pelaaja halua aloittaa uutta peliä
+# Jos on uusi pelaaja niin aloitetaan uusi peli
+while True:
+    vastaus = input("Oletko uusi pelaaja? (kyllä/ei): ")
+    if vastaus == "kyllä":      # eli uusi pelaaja
+        while True:
+            nimimerkki = input("Anna lentävälle apinalle nimimerkki: ")
+            if nimimerkki_käytössä(nimimerkki):
+                print("Nimimerkki on jo käytössä, valitse toinen.")
+            else:
+                lisää_pelaaja(nimimerkki)
+                break
+        break
+
+    elif vastaus == "ei":       # eli vanha pelaaja
+        while True:
+            jatka_peliä = input("Haluatko jatkaa mihin jäit? (kyllä/ei): ")
+            if jatka_peliä == "kyllä":
+                nimimerkki = input("Anna vanha nimimerkkisi: ")
+                if nimimerkki_käytössä(nimimerkki):
+                    print(f"Tervetuloa takaisin peliin, {nimimerkki}!")
+                    break
+                else:
+                    print("Nimimerkkiä ei löytynyt.")
+            elif jatka_peliä == "ei":
+                while True:
+                    nimimerkki = input("Anna uusi nimimerkki: ")
+                    if nimimerkki_käytössä(nimimerkki):
+                        print("Nimimerkki on jo käytössä, valitse toinen.")
+                    else:
+                        lisää_pelaaja(nimimerkki)
+                        break
+                break
+
+            else:
+                print('Väärä syöte, kirjoita vain "kyllä" tai "ei".')
+        break
+
+
+    else:
+        print('Väärä syöte, kirjoita vain "kyllä" tai "ei".')
